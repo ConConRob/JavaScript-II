aae5f2dc0afc35508b6361967322550319e84bb9
conconrob <connortprobertson@gmail.com>
2019-01-22 12:52:25 +0000

    done closure challenge

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let outside ="i am outsite the function";
+function closure() {
+  let inside = "I am inside this function";
+  console.log(outside);
+  console.log(inside) ;
+};
+//console.dir(closure);
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count =0;
+
+  return function () {
+    console.log(count);
+    return count++;
+  } 
 };
+const counterOne = counter();
+counterOne();
+counterOne();
+counterOne();
+counterOne();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
