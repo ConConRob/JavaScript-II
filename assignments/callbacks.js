5bed5e66c385bfe5ab0e374614da01c93e14bbee
conconrob <connortprobertson@gmail.com>
2019-01-22 13:13:38 +0000

    done callbacks stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -62,4 +62,9 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  cb(array.filter( (obj, i )=> array.indexOf(obj) ===i
+  ));
 }
+items.push(...items);
+console.log(items);
+removeDuplicates(items, logger);
